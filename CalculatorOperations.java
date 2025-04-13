eec7f817ea10d814ec8238c5c9c53bd8d1201ec0
Gaurab <gaurabkus781@gmail.com>
2025-04-13 21:31:26 +0530

    Create addition function

--- CalculatorOperations.java
+++ CalculatorOperations.java
@@ -3,5 +3,19 @@ import java.util.Scanner;
 public class CalculatorOperations {
      Scanner scanner = new Scanner(System.in);
 
-     
+    /**
+     * Adds two numbers provided by user input.
+     * @return Sum of the two numbers
+     */
+    public double add() {
+        // Prompt for two numbers
+        System.out.print("Enter first number for addition: ");
+        double a = Double.parseDouble(scanner.nextLine());
+        System.out.print("Enter second number for addition: ");
+        double b = Double.parseDouble(scanner.nextLine());
+
+        return a + b;
+    }
+
+
 }
